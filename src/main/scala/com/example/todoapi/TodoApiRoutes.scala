8403685fa2441f0fd0428e040d7342c3b024adce
Simon Walenkamp <simonwalenkamp@hotmail.com>
2025-07-13 13:28:02 +0200

    adjust response codes

--- src/main/scala/com/example/todoapi/TodoApiRoutes.scala
+++ src/main/scala/com/example/todoapi/TodoApiRoutes.scala
@@ -17,26 +17,26 @@ object TodoApiRoutes:
       case GET -> Root / UUIDVar(userId) / "todos" =>
         todoService.getAllTodoLists(userId).flatMap {
           case Right(todoLists) => Ok(GetAllTodoListsResponse(todoLists))
-          case Left(error) => BadRequest(error.getMessage)
+          case Left(error) => NotFound(error.getMessage)
         }
 
       case POST -> Root / UUIDVar(userId) / "todos" =>
         todoService.createTodoList(userId).flatMap( {
-          case Right(id) => Ok(CreateTodoListResponse(id))
-          case Left(error) => BadRequest(error.getMessage)
+          case Right(id) => Created(CreateTodoListResponse(id))
+          case Left(error) => NotFound(error.getMessage)
         })
 
       case GET -> Root / UUIDVar(userId) / "todos" / UUIDVar(id) =>
         todoService.getTodoList(userId, id).flatMap( {
           case Right(todoList) => Ok(GetTodoListResponse.fromTodoList(todoList))
-          case Left(error) => BadRequest(error.getMessage)
+          case Left(error) => NotFound(error.getMessage)
         })
 
       case req @ POST -> Root / UUIDVar(userId) / "todos" / UUIDVar(listId) =>
         req.as[AddTodoRequest].flatMap { addRequest =>
           todoService.addTodo(userId, listId, addRequest.name).flatMap {
-            case Right(id) => Ok(CreateTodoResponse(id))
-            case Left(error) => BadRequest(error.getMessage)
+            case Right(id) => Created(CreateTodoResponse(id))
+            case Left(error) => NotFound(error.getMessage)
           }
         }
 
@@ -44,13 +44,13 @@ object TodoApiRoutes:
         req.as[UpdateTodoRequest].flatMap { updateRequest =>
           todoService.updateTodo(userId, listId, todoId, updateRequest.name, updateRequest.status).flatMap {
             case Right(todo) => Ok(TodoResponse.fromTodo(todo))
-            case Left(error) => BadRequest(error.getMessage)
+            case Left(error) => NotFound(error.getMessage)
           }
         }
 
       case DELETE -> Root / UUIDVar(userId) / "todos" / UUIDVar(todoListId) / UUIDVar(id) =>
         todoService.deleteTodo(userId, todoListId, id).flatMap( {
           case Right(_) => NoContent()
-          case Left(error) => BadRequest(error.getMessage)
+          case Left(error) => NotFound(error.getMessage)
         })
     }
